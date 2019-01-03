7a8dc0caad1782f4e85a164391a2be6a190b1078
alexwindelberg <alexwindelberg@gmail.com>
2019-01-03 14:55:34 -0500

    adding Subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -27,13 +27,14 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription 
+store.subscribe(() => {
+    console.log('[Subcription]', store.getState());
+});
+
 // Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
 
 
-// Subscription 
-store.subcribe(() => {
-    console.log('[Subcription]', store.getState());
-});
